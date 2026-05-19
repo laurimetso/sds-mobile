58d3ce1d4e32ad0430ee19e798e71ba33a91036c
Lauri <lauri.metso@student.lut.fi>
2026-05-19 19:18:56 +0300

    third commit

--- app/src/main/java/com/example/sds_exercises/SecondActivity.java
+++ app/src/main/java/com/example/sds_exercises/SecondActivity.java
@@ -1,11 +1,21 @@
 package com.example.sds_exercises;
 
+import android.content.Intent;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+
+    ListView myListView;
+    String[] items;
+    String[] prices;
+    String[] descriptions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +29,26 @@ public class SecondActivity extends AppCompatActivity {
             tv.setText(text);
 
         }*/
+
+        Resources res = getResources();
+        myListView = findViewById(R.id.myListView);
+        items = res.getStringArray(R.array.items);
+        prices = res.getStringArray(R.array.prices);
+        descriptions = res.getStringArray(R.array.descriptions);
+
+        ItemAdapter itemAdapter = new ItemAdapter(this,items,prices,descriptions);
+        myListView.setAdapter(itemAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long id) {
+                Intent showDetailActivity = new Intent(getApplicationContext(), DetailActivity.class);
+                showDetailActivity.putExtra("com.example.sds_exercises.ITEM_INDEX",i);
+                startActivity(showDetailActivity);
+            }
+        });
+
+
+
     }
 }
